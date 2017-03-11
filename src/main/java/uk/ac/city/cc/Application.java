dd58ff959f390fdd62fd9d12375e2e812093ba78
Santiago Ginzburg <santiagoginzburg@Santiagos-MacBook-Pro.local>
2017-03-11 11:33:48 +0000

    start building gradle project

---
+++ src/main/java/uk/ac/city/cc/Application.java
@@ -0,0 +1,12 @@
+package uk.ac.city.cc;
+
+/**
+ * Created by santiagoginzburg on 11/03/2017.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
